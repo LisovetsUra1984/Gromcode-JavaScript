114299b83e88f5d930cb543b8da92bae5f8d365a
Ura Lisovets <lisovetsura1984@gmail.com>
2020-01-02 15:49:29 +0200

    lessons2/task4

--- lessons2/task4/index.js
+++ lessons2/task4/index.js
@@ -2,7 +2,3 @@ const  a = 4;
 const  b = 2;
 console.log('a: ',a);
 console.log('b: ',b); 
-console.log('a - b = ',a - b);
-console.log('a + b = ',a + b);
-console.log('a / b = ',a / b);
-console.log('a * b = ',a * b);
