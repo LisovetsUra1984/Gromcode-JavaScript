a8c01fe50ca9795e54595ee7be74617c4acfec0f
Ura Lisovets <lisovetsura1984@gmail.com>
2020-01-04 23:18:57 +0200

    task4

--- lessons3/task1/task4/index.js
+++ lessons3/task1/task4/index.js
@@ -2,7 +2,7 @@
 false || true;
 console.log("false || true: ",false || true);
 false || true || false;
-console.log("false || true || false;: ",false || true || false);
+console.log("false || true || false: ",false || true || false);
 false || false || false;
 console.log("false || false || false: ", false || false || false);
 
@@ -30,7 +30,7 @@ console.log("undefined || null || 0 || '' || null: ", undefined || null || 0 || 
 const customAmount = null; // не выводим
 const defaultAmount = 17; // не выводим
 const amount = customAmount || defaultAmount; // выводим значкние переменной amount в формате console.log('amount: ', amount);
-console.log('amount||defaultAmount: ', amount||defaultAmount);
+console.log('amount||amount: ', amount||amount);
 
 /* && находит первое ложное значение */
 'text' && false;
@@ -38,7 +38,7 @@ console.log("'text' && false: ", 'text' && false);
 true && 'text';
 console.log("true && 'text': ", true && 'text');
 undefined && null && 0 && '' && null;
-console.log("undefined || null || 0 || '' || null: ", undefined || null || 0 || '' || null);
+console.log("undefined && null && 0 && '' && null: ", undefined && null && 0 && '' && null);
 
 /* оператор отрицания (OR) */
 !true;
