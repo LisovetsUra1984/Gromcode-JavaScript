9648d68aeff03abcad03aeb414345e147e951111
Ura Lisovets <lisovetsura1984@gmail.com>
2020-01-04 21:16:29 +0200

    task3

---
+++ lessons3/task1/task2/task3/index.js
@@ -0,0 +1,48 @@
+/* сравнение чисел */
+
+const a = 17;
+const b = 5;
+a > b;
+console.log("a > b: ", a > b);
+a <= b;
+console.log("a <= b: ", a <= b);
+a == b;
+console.log("a == b: ", a == b);
+a != b;
+console.log("a != b: ", a != b);
+
+/* сравнение строк */
+
+'a' < 'b';
+console.log("'a' < 'b': ", 'a' < 'b');
+'ab' > 'a';
+console.log("'ab' > 'a': ", 'ab' > 'a');
+/* строгое сравнение */
+
+17 === 1;
+17 === 17;
+17 === '17';
+17 === true;
+'0' === '';
+true === false;
+true === true;
+null === undefined;
+false === 0
+
+
+/* нестрогое сравнение */
+
+17 == '17';
+'0' == '';
+0 == '';
+null == undefined;
+false == 0;
+
+
+/* осторожно c null и undefined */
+
+undefined == null;
+undefined == 0;
+null == 0;
+undefined < 0;
+undefined > 0;
