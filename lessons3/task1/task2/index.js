afde2eacf4f194d42484c9fcb78f669b31aab8a3
Ura Lisovets <lisovetsura1984@gmail.com>
2020-01-04 18:35:21 +0200

    task2

--- lessons3/task1/task2/index.js
+++ lessons3/task1/task2/index.js
@@ -13,4 +13,8 @@ console.log("'   20   ' to number is " + Number('   20   '));
 console.log("'      ' to number is " + Number('      '));
 console.log("'   30d   ' to number is " + Number('   30d   '));
 console.log('null to boolean is ' + Boolean(null));
+console.log('undefined to boolean is ' + Boolean(undefined));
+console.log('0 to boolean is ' + Boolean(0));
+console.log(' -0 to boolean is ' + Boolean( -0));
+console.log(' NaN to boolean is ' + Boolean( NaN));
 console.log('\'Hello\' to boolean is ' + Boolean('Hello'));
