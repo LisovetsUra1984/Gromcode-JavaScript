d92bd4ae71c99eabcd9ccdf09b2c2298fe302f0c
Ura Lisovets <lisovetsura1984@gmail.com>
2020-01-04 14:07:38 +0200

    task1

--- lessons3/task1/index.js
+++ lessons3/task1/index.js
@@ -25,9 +25,9 @@ let num = 17;
 
 // Постфиксная и префиксная форма записи
 ++num;
-console.log("undefined + '5': ", undefined + '5');
+console.log("++num: ",++num);
 num++;
-console.log("undefined + '5': ", undefined + '5');
+console.log("num++: ", num++);
 
 // математические операторы приводят операнды к числам
 const a = 17;
