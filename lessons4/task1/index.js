b2b2eabf77f550af2d69459871c6a52c9589cce4
Ura Lisovets <lisovetsura1984@gmail.com>
2020-01-08 15:46:14 +0200

    task1

--- lessons4/task1/index.js
+++ lessons4/task1/index.js
@@ -1,7 +1,7 @@
 let n = 100;
-let sum = 1;
-while (n <=100){
-sum +=n;
-n += 1;
-console.log('Result: ' + sum);
+let sum = 0;
+while (n <= 100) {
+    sum += n;
+    n += 1;
+    console.log('Result: ' + sum);
 } 
