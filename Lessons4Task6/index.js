a5e9cdd106a64cd448be952c3f0e8331fba25b87
Ura Lisovets <lisovetsura1984@gmail.com>
2020-01-10 17:12:55 +0200

    task6

--- Lessons4Task6/index.js
+++ Lessons4Task6/index.js
@@ -1,19 +1,19 @@
 let sum = 0;
 for(let  i = 0; i < 1000; i++) {
-if (i % 2 == 0){
-console.log('Faund');
-sum = sum + i;
+  if (i % 2 == 0){
+    console.log('Faund');
+    sum = sum + i;
   }
 }
  if (sum * 5 > 5000) 
   {
-console.log('Bigger')
+ console.log('Bigger')
   }
-if(sum * 5 > 5000)
+else if(sum * 5 > 5000)
 {
   console.log('Smaller or equal');
 }
- if (sum * 5 == 5000)
+ else if (sum * 5 == 5000)
 {
  console.log('Smaller or equal'); 
 }
