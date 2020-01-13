a8dbcafd4060007bf6451b8fbd8b99e54972ca2d
Ura Lisovets <lisovetsura1984@gmail.com>
2020-01-13 18:29:00 +0200

    task5

--- Lessons4Task5/index.js
+++ Lessons4Task5/index.js
@@ -1,6 +1,7 @@
 let m = 1;
 const n = 50;
 let result = 0;
+let t = i;
 for(let i = m;i <= n;i++)
 {
 if(i % 2== 0 & i % 4 !=0)
@@ -17,7 +18,7 @@ result = result * i;
 }
 else if(i % 5==0)
 {
-console.log(i)
+console.log(t)
 }
 
 }
