2de7b2e7e695441026ee1a94799f0958f8df3f5c
Ura Lisovets <lisovetsura1984@gmail.com>
2020-01-23 21:58:44 +0200

    task8

--- Lessons5Task8/index.js
+++ Lessons5Task8/index.js
@@ -1,22 +1,22 @@
 function compareSums(a,b,c,d)
 {
-  if(sum(a,b)>sum(c,d))
-  {
-    return true;
-  }
-  else if(sum(a,b)<sum(c,d))
-  {
-    return false;
-  }
+if(sum(a,b)>sum(c,d))
+{
+return true;
+}
+else if(sum(a,b)<sum(c,d))
+{
+return false;
+}
 }
 function sum(a,b)
 {
-  res = a;
-    do{
-        a++
-    res = res+a;
-    }
-    while(a<b)
+res = a;
+do{
+a++
+res = res+a;
+}
+while(a<b);
   
-  return res;
+return res;
 }
